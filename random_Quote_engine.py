d3fb4207946b60469cb3369c17bac83bd663a1e5
Ankita-Tode <83173391+Ankita-Tode@users.noreply.github.com>
2024-04-23 16:12:03 +0530

    Create random_Quote_engine.py

---
+++ random_Quote_engine.py
@@ -0,0 +1,7 @@
+import random
+def sentence(fruits,reason):
+  print("I like most",random.choice(fruits),random.chioce(reason))
+def main():
+  fruits=["apple", "banana","orange"]
+  reason=["keep doctor away","build imunity","having more antioxident"]
+  sentence(fruits,reason)
